fee2658bfd9d88b9c6f7aa33d9525f76ef695490
kolecki4 <55369795+kolecki4@users.noreply.github.com>
2021-03-16 13:32:41 -0400

    Update Settings.py

--- EWC/Settings.py
+++ EWC/Settings.py
@@ -1,21 +1,13 @@
 import numpy as np
 import astropy.io.fits as fits
 
-
-#=============================================================================
-# This program does most of the heavy-lifting of measuring absorption lines for you.
-# It calculates the equivalent width of all the given lines in the spectrum supplied
-# in units of milliAngstroms with graphs for verification purposes. 
-
-# CHANGE THE PARAMETERS BELOW ACCORDINGLY
-#=============================================================================
+==============================================================
 
 # FULL PATH OF SPECTRUM IN FITS FORMAT
 #filename = "/media/jared/Acer/Users/jared/Desktop/ResearchFolder/spectra/BD+183423bspectrum.dxt.nor"
 filename='1D_Spec_HR7672.txt'
 
 # OPEN THE 1D SPECTRUM FILE HERE (MAKE SURE YOU IMPORT ANY AND ALL LIBRARIES NECESSARY)
-
 #fitsfile= fits.open(filename')
 wav =np.genfromtxt(filename, usecols=0)
 flx =np.genfromtxt(filename, usecols=1)
@@ -37,11 +29,6 @@ autoDiscard = False
 # GENERALLY USE 0.5
 FWHeight = 0.5
 
-# INCREASE halfwindow TO SEARCH A WIDER RANGE FOR THE MIN FLUX VALUES
-# (USEFUL IF THE SPECTRUM IS NOT RV OR REDSHIFT ADJUSTED, FOR EXAMPLE)
-dlambda = np.average([wav[i+1] - wav[i] for i in range(len(wav)-1)])
-halfwindow = int(.35/dlambda)
-
 # MINIMUM EQW IN MILLIANGSTROMS TO BE OUTPUT TO THE LINE LIST FILE
 # (MEASUREMENTS OF LINES WEAKER THAN THIS ARE CONSIDERED TO BE TOO NOISY FOR USE)
 mineqw = 5
@@ -59,3 +46,9 @@ colnames = ('Element', 'Ionization', 'Wavelength', 'ExcitationPot', 'Loggf')
 linelist = np.genfromtxt(linelistname, dtype = None, encoding = None, names = colnames, skip_header = 16, usecols = [0,1,2,3,4])
 elnums = np.array([2,3,6,7,8,11,12,13,14,16,20,22,23,24,26,28,39,40,56,63])
 elnames = np.array(['He','Li','C','N','O','Na','Mg','Al','Si','S','Ca','Ti','V','Cr','Fe','Ni','Y','Zr','Ba','Eu'])
+
+
+# INCREASE halfwindow TO SEARCH A WIDER RANGE FOR THE MIN FLUX VALUES
+# (USEFUL IF THE SPECTRUM IS NOT RV OR REDSHIFT ADJUSTED, FOR EXAMPLE)
+dlambda = np.average([wav[i+1] - wav[i] for i in range(len(wav)-1)])
+halfwindow = int(.35/dlambda)
